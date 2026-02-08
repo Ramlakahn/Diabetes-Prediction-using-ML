365d0cb71370eeef511ee54d5b5ac50df07db366
Shivansh yadav <87001528+Ramlakahn@users.noreply.github.com>
2026-02-08 23:07:31 +0530

    Delete AI.py

--- AI.py
+++
@@ -1,9 +0,0 @@
-class InvalidAgeError(Exception):
-    pass
-
-try:
-    age = int(input("Enter age: "))
-    if age < 18:
-        raise InvalidAgeError("Age must be 18 or older!")
-except InvalidAgeError as e:
-    print(f"Error: {e}")
